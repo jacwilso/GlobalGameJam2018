342027a5923ed92a3f79497eb54dc19c7713d972
Jacob Wilson <jacwilso0@gmail.com>
2018-01-27 11:52:59 -0500

    Menu and fonts

---
+++ GlobalGameJam_Unity/Assets/_Scripts/Helpers/MenuManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public const string Main_Scene = "main";
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(Main_Scene);
+    }
+}
---
+++ GlobalGameJam_Unity/Assets/_Scripts/Tram/MenuTramSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTramSpawner : MonoBehaviour {
+
+    [SerializeField] private GameObject Tram;
+    [SerializeField] private float spawnRate;
+
+	private void Start () {
+		
+	}
+	
+	private void Update () {
+		
+	}
+}
