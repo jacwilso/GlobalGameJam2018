44bb7e8107b071b7a757a25c3f4be6b42815b251
Jacob Wilson <jacwilso0@gmail.com>
2018-01-27 11:52:59 -0500

    Auto stash before merge of "master" and "origin/master"

--- GlobalGameJam_Unity/Assets/_Scripts/Tram/Tram.cs
+++ GlobalGameJam_Unity/Assets/_Scripts/Tram/Tram.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(SplineWalker), typeof(Rigidbody), typeof(BoxCollider))]
 public class Tram : MonoBehaviour {
 
     private bool pathSelected, offscreen;
