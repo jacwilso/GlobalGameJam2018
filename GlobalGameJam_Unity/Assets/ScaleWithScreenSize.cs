750c51882102d2e23455c7096886669ed7ee8458
Jacob Wilson <jacwilso0@gmail.com>
2018-01-28 02:35:37 -0500

    UI maximize and Trees

---
+++ GlobalGameJam_Unity/Assets/ScaleWithScreenSize.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleWithScreenSize : MonoBehaviour {
+
+	private void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
